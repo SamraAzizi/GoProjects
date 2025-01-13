c64737f5899f2f5ff4011c99f68ba7cc6ebeb4bf
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-01-13 17:23:32 +0500

    webServer

---
+++ webServer/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello")
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handler("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8000\n")
+	if err := http.ListenServer(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
+}

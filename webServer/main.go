a494d43bbf989efa5a487bae3457149d8802f89a
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-01-13 17:39:32 +0500

    webServer

--- webServer/main.go
+++ webServer/main.go
@@ -6,8 +6,21 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err : %d", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.path != "/hello" {
+	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}

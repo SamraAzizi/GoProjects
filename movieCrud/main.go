b6e7d398ac8546f743c20573c0f279ecc058782e
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-01-14 22:08:34 +0500

    movieCrude

--- movieCrud/main.go
+++ movieCrud/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"github.com/gorrila/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFux("/movies", getMovies).Method("GET")
+	r.handleFunc("/movies")
+}

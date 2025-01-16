d690508083727add939c6070c45621f2da489881
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-01-16 19:36:50 +0500

    movieCrude

--- movieCrud/main.go
+++ movieCrud/main.go
@@ -65,7 +65,7 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func updateMovie() {
+func updateMovie(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "applicaiton/json")
 	//params
@@ -77,7 +77,13 @@ func updateMovie() {
 	for index, item := range movies {
 		if item.ID == params["id"] {
 			movies = append(movies[:index], movies[index+1:]...)
-			var mvoie Movie
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+
 		}
 	}
 

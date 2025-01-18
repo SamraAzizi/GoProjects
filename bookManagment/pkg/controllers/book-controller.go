9e4528bab8c11b6a4c77ad298aeaab79e7263414
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-01-18 12:30:19 +0500

    book

--- bookManagment/pkg/controllers/book-controller.go
+++ bookManagment/pkg/controllers/book-controller.go
@@ -2,10 +2,13 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/SamraAzizi/GoProjects/pkg/models"
 	"github.com/akhil/go-bookstore/pkg/models"
+	"github.com/gorilla/mux"
 )
 
 var newBook models.Book
@@ -16,4 +19,14 @@ func GetBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	bookDetail, _ := models.GetBookById(ID)
 }

a16f3763392aa5caccf713a709d09d5f0ae246e7
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-01-19 12:15:53 +0500

    book

--- bookManagment/pkg/controllers/book-controller.go
+++ bookManagment/pkg/controllers/book-controller.go
@@ -29,4 +29,24 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("error while parsing")
 	}
 	bookDetail, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetail)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	CreateBook := &model.Book()
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv
 }

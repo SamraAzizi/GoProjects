3abe038f14572f6ea3be8c8dc1dd333ea6e26721
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-01-21 20:49:33 +0500

    slackCommand

--- slackBot/main.go
+++ slackBot/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/shomali11/slacker"
 )
 
-func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
-	for event := range analyticsChannel {
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent){
+	for event := range analyticsChannel{
 		fmt.Println("Command Events")
 		fmt.Println(event.Timestamp)
 		fmt.Println(event.Command)
@@ -19,8 +21,25 @@ func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 }
 
 func main() {
-
+	
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOEKN"))
 
+
 	go printCommandEvents(bot.printCommandEvents())
+
+	bot.Command("my yob is <year>", &slacker.CommandDefinition){
+		Description : "yob Calculator",
+		Example : "my yob is 2025",
+		Handler : func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter){
+			year := request.Param("year")
+		}
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil{
+		log.Fatal(err)
+	}
 }

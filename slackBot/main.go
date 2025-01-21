a42b0c3b669aad974d0d99175946adf14494a610
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-01-21 20:28:54 +0500

    slackCommands

--- slackBot/main.go
+++ slackBot/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/shomali11/slacker"
 )
 
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
+
 func main() {
 
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOEKN"))
+
+	go printCommandEvents(bot.printCommandEvents())
 }
-fmt"aspdgh

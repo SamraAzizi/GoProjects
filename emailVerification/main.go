737c5dc16aeac08bae18ba5a37e24749991dfec1
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-01-22 21:47:03 +0500

    email

--- emailVerification/main.go
+++ emailVerification/main.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -25,5 +27,32 @@ func checkDomain(domain string) {
 
 	var hasMX, hasSPF, hasDMARC bool
 	var spfRecord, dmarcRecord string
+
+	mxRecords, err := net.LookupMX(domain)
+
+	if err != nil {
+		log.Printf("error: %v\n", err)
+
+	}
+
+	if len(mxRecords) > 0 {
+		hasMX = true
+	}
+
+	txtRecords, err := net.LookupTXT(domain)
+
+	if err != nil {
+		log.Printf("error: %v\n", err)
+
+	}
+
+	for _, record := range txtRecords {
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+
+	}
 
 }

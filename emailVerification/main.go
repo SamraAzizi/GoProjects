529ce019d1ba04912c5a51b90acfd0b2ec8e8760
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-01-22 21:35:22 +0500

    email

---
+++ emailVerification/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("domain, hasMX, hasSPF, sprRecord, hasDMARC, dmarcRecord\n")
+
+}
+
+func checkDomain(domain string) {
+
+}

39e2ada5ea9f8bc1abece7fb9757e28d0baaa256
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-01-23 22:13:51 +0500

    waether

---
+++ weather/main.go
@@ -0,0 +1,12 @@
+package main
+
+type apiConfigData struct {
+	OpenWeatherMapApiKey string `json:"OpenWeatherMapApiKey"`
+}
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}

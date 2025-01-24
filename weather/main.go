6c61bc80ab9f636a67f5bbfd2e41a1f23ef9d2d1
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-01-24 20:14:06 +0500

    weather

--- weather/main.go
+++ weather/main.go
@@ -35,16 +35,22 @@ func loadApiConfig(filename string) (apiConfigData, error) {
 
 }
 
-
-func main(){
+func main() {
 	http.HandleFunc("/hello", hello)
 
 	http.HandleFunc("/weather/",
-	func(w http.ResponseWriter, r *http.Request){
-		city := strings.SplitN(e.URL.Path, "/", 3)[2]
-		query(city)
-	}
-)
+		func(w http.ResponseWriter, r *http.Request) {
+			city := strings.SplitN(e.URL.Path, "/", 3)[2]
+			data, err := query(city)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+
+			}
+
+			w.Header().Set("contentType", "application/json; charset-utf-8")
+			json.NewEncoder(w).Encode(data)
+		})
 
 	http.ListenAndServe(":8000", nil)
 }

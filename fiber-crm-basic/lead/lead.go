fd7d243f5eb34e02b7d7f1af62514661791d09e8
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-01-28 19:52:24 +0500

    fiber

--- fiber-crm-basic/lead/lead.go
+++ fiber-crm-basic/lead/lead.go
@@ -1,4 +1,33 @@
 package lead
 
+import (
+	"github.com/akhil/go-fiber-crm-basic/database"
+	"github.com/jinzhu/gorm"
+)
+
 type Lead struct {
+	gorm.Model
+	Name    string
+	Company string
+	Email   string
+	Phone   int
+}
+
+func GetLeads(c *fiber.Ctx) {
+
+}
+
+func GetLead(c *fiber.Ctx) {
+	id := c.Params("id")
+	db := database.DBConn
+	var lead lead
+	db.JSON(lead)
+}
+
+func NewLead(c *fiber.Ctx) {
+
+}
+
+func DeleteLead(c *fiber.Ctx) {
+
 }
